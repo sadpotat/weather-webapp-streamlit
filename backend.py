ba412502117a895ad597b74fadcef82baffa340d
sadpotat <sadia.tasneem16@gmail.com>
2023-04-26 22:18:09 +0600

    added content to today tab

--- backend.py
+++ backend.py
@@ -1,20 +1,27 @@
 import requests
+from pprint import pprint
 
 with open("api_key.txt", "r") as file:
     API_KEY = file.read()
 
 
-def get_data(place, forecast_days):
+def get_data(place):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     content = response.json()
     filtered_data = content["list"]
-    num_values = 8*forecast_days
-    filtered_data = filtered_data[:num_values]
-    return filtered_data
+    filter_by_day = {}
+    for datapoint in filtered_data:
+        date = datapoint["dt_txt"][:10]
+        if date in filter_by_day.keys():
+            filter_by_day[date].append(datapoint)
+        else:
+            filter_by_day[date] = []
+            filter_by_day[date].append(datapoint)
+    return filter_by_day
 
 
 if __name__ == "__main__":
     # pprint(get_data("Tokyo", None, None))
-    data = get_data("Tokyo", 3)
-    print(data)
+    data = get_data("Tokyo")
+    pprint(data)
--- main.py
+++ main.py
@@ -2,42 +2,100 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 import streamlit as st
+from datetime import datetime as dt
+
 
 st.set_page_config(page_title="Weather",
                    page_icon=":sun_with_face:", layout="wide")
 
-st.title("Weather Forecast for the Next Days")
-location = st.text_input("City/Country: ")
-days = st.slider("Forecast Days", min_value=1, max_value=5,
-                 help="Select the number of forecasted days")
-option = st.selectbox("Select data to view", ("Temperature", "Sky",))
-st.subheader(f"{option} for the next {days} days in {location}")
-
+location = st.text_input("City/Country: ", placeholder="Enter a location",
+                         help="Enter a city or country's name to view its weather forecast")
 
 if location:
-    # get temperature/sky data
     while True:
         try:
-            data = get_data(location, days)
+            data = get_data(location)
             break
         except:
             st.warning('The City/Country does not exist!', icon="⚠️")
             # st.write("City/Country does not exist")
             st.stop()
 
-    if option == "Temperature":
-        temperatures = [reading["main"]["temp"] /
-                        10 for reading in data]
-        dates = [reading["dt_txt"] for reading in data]
-        # Create temperature plot
-        figure = px.line(x=dates, y=temperatures, labels={
-            "x": "Date", "y": "Temperature"})
-        # gets figure object as input. Alternate libraries: Plotly, Bokeh
-        st.plotly_chart(figure)
-    elif option == "Sky":
-        sky_conditions = [reading["weather"][0]["main"]
-                          for reading in data]
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        imagepaths = [images[condition] for condition in sky_conditions]
-        st.image(imagepaths, width=100)
+    today, tomorrow, fivedays = st.tabs(["Today", "Tomorrow", "Next 5 days"])
+    images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+              "Rain": "images/rain.png", "Snow": "images/snow.png"}
+    now = dt.now()
+
+    with today:
+        st.title(now.strftime("%B %d, %I:%M %p"))
+        date_today = now.strftime("%Y-%m-%d")
+        todays_data = data[date_today]
+        sky_conditions = []
+
+        # css tags to remove delta arrow from st.metric()
+        st.write(
+            """
+            <style>
+            [data-testid="stMetricDelta"] svg {
+                display: none;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True,
+        )
+
+        for point in todays_data:
+            temp = point["main"]["temp"]
+            feel = point["main"]["feels_like"]
+            sky_condition = point["weather"][0]["main"]
+
+            col1, mid, col2 = st.columns([1, 1, 1])
+
+            with col1:
+                st.metric("Temperature", f"{(temp/10):.2f} °C", delta=f"Feels like {(feel/10):.2f} °C",
+                          delta_color="off", help=None, label_visibility="visible", )
+
+            with col2:
+                st.image(images[sky_condition],
+                         width=175, caption=sky_condition)
+
+    with tomorrow:
+        st.title("Tomorrow's Weather Forecast")
+
+    with fivedays:
+        st.title("Weather Forecast for the Next Five Days")
+
+# st.title("Weather Forecast for the Next Days")
+# location = st.text_input("City/Country: ")
+# days = st.slider("Forecast Days", min_value=1, max_value=5,
+#                  help="Select the number of forecasted days")
+# option = st.selectbox("Select data to view", ("Temperature", "Sky",))
+# st.subheader(f"{option} for the next {days} days in {location}")
+
+# if location:
+#     # get temperature/sky data
+#     while True:
+#         try:
+#             data = get_data(location, days)
+#             break
+#         except:
+#             st.warning('The City/Country does not exist!', icon="⚠️")
+#             # st.write("City/Country does not exist")
+#             st.stop()
+
+#     if option == "Temperature":
+#         temperatures = [reading["main"]["temp"] /
+#                         10 for reading in data]
+#         dates = [reading["dt_txt"] for reading in data]
+#         # Create temperature plot
+#         figure = px.line(x=dates, y=temperatures, labels={
+#             "x": "Date", "y": "Temperature"})
+#         # gets figure object as input. Alternate libraries: Plotly, Bokeh
+#         st.plotly_chart(figure)
+#     elif option == "Sky":
+#         sky_conditionss = [reading["weather"][0]["main"]
+#                           for reading in data]
+#         images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+#                   "Rain": "images/rain.png", "Snow": "images/snow.png"}
+#         imagepaths = [images[condition] for condition in sky_conditionss]
+#         st.image(imagepaths, width=100)
